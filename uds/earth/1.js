7bc5b97ffbca82865f5a866de9688511c054f8ba
ljcool2006 <51834343+ljcool2006@users.noreply.github.com>
2025-02-18 09:06:11 -0500

    attempt 2

---
+++ uds/earth/1.js
@@ -0,0 +1,9 @@
+if (window['google'] != undefined && window['google']['loader'] != undefined) {
+if (!window['google']['earth']) {
+window['google']['earth'] = {};
+google.earth.Version = '1.0';
+google.earth.JSHash = 'd2fd21686addcd75dd267a0ff2f7b381';
+google.earth.LoadArgs = 'file\x3dearth\x26v\x3d1';
+}
+google.loader.writeLoadTag("script", google.loader.ServiceBase + "/api/earth/1.0/d2fd21686addcd75dd267a0ff2f7b381/default.I.js", false);
+}
